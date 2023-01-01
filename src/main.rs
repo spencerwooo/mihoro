ec2538f9f4bd139baa0593ff8495a3aa4e3b07a8
spencerwooo <spencer.woo@outlook.com>
2023-01-01 21:45:22 +0800

    separate into mods

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
+mod utils;
+
 use clap::{Parser, Subcommand};
 use colored::*;
-use flate2::read::GzDecoder;
-use serde::{Deserialize, Serialize};
-use toml;
-
-use std::{fs, io, path::Path};
+use utils::*;
 
 #[derive(Parser)]
 #[command(author, about, version)]
 struct Args {
     /// Path to clashrup config file
-    #[clap(short, long, default_value = "config.toml")]
+    #[clap(short, long, default_value = "clashrup.toml")]
     clashrup_config: String,
 
     #[command(subcommand)]
@@ -23,98 +21,37 @@ enum Commands {
     Setup,
 }
 
-#[derive(Serialize, Deserialize)]
-struct Config {
-    remote_clash_binary_url: String,
-    remote_config_url: String,
-    clash_config_root: String,
-}
-
-fn setup_default_config(path: &String) {
-    let default_config = Config {
-        remote_clash_binary_url: String::from(""),
-        remote_config_url: String::from(""),
-        // Reference to clash config: https://github.com/Dreamacro/clash/wiki/configuration
-        clash_config_root: String::from("~/.config/clash"),
-    };
-    let config = toml::to_string(&default_config).unwrap();
-    fs::write(path, config).unwrap();
-}
-
-fn parse_config(path: &String) -> Config {
-    let config = fs::read_to_string(path).unwrap();
-    let config: Config = toml::from_str(&config).unwrap();
-    config
-}
-
-fn download_file(url: &String, path: &String) {
-    println!(
-        "{} Downloading from {}",
-        "download:".bold().blue(),
-        url.underline().yellow()
-    );
-    let mut resp = reqwest::blocking::get(url).unwrap();
-    let mut file = fs::File::create(path).unwrap();
-    resp.copy_to(&mut file).unwrap();
-    println!(
-        "{} Downloaded to {}",
-        "download:".bold().blue(),
-        path.underline().yellow()
-    );
-}
-
-fn extract_gzip(gzip_path: &String, filename: &String) {
-    let mut archive = GzDecoder::new(fs::File::open(gzip_path).unwrap());
-    let mut file = fs::File::create(filename).unwrap();
-    io::copy(&mut archive, &mut file).unwrap();
-    fs::remove_file(gzip_path).unwrap();
-    println!(
-        "{} Extracted to {}",
-        "clashrup:".bold().green(),
-        filename.underline().yellow()
-    );
-}
-
-fn validate_clashrup_config(path: &String, output_prefix: &str) -> Config {
-    // Create clashrup default config if not exists
-    let config_path = Path::new(path);
-    if !config_path.exists() {
-        setup_default_config(path);
-        panic!(
-            "{} Config file not found, created default one at {}",
-            output_prefix,
-            path.underline().yellow()
-        );
-    }
-
-    // Parse config file and validate if urls are defined
-    println!(
-        "{} Reading config file {}",
-        output_prefix,
-        path.underline().yellow()
-    );
-    let config = parse_config(path);
-    if config.remote_clash_binary_url.is_empty() {
-        panic!(
-            "{} Please set remote_clash_binary_url in config file",
-            output_prefix.red()
-        );
-    }
-    if config.remote_config_url.is_empty() {
-        panic!(
-            "{} Please set remote_config_url in config file",
-            output_prefix.red()
-        );
-    }
-    return config;
-}
-
 fn main() {
     let args = Args::parse();
-    let prefix = "clashrup:".bold().cyan();
+    let prefix = "clashrup:";
 
     // Initial setup and parse config file
-    let config = validate_clashrup_config(&args.clashrup_config, &prefix);
+    let config: Config = match validate_clashrup_config(&args.clashrup_config, &prefix) {
+        Ok(config) => config,
+        Err(error) => {
+            match error {
+                ClashrupConfigError::ConfigMissingError => {
+                    println!("{} Created default config, edit as needed", prefix.yellow());
+                    println!("{} Run again to finish setup", prefix.yellow());
+                }
+                ClashrupConfigError::RemoteClashBinaryUrlMissingError => {
+                    println!(
+                        "{} Missing {}",
+                        "error:".red(),
+                        "remote_clash_binary_url".underline()
+                    );
+                }
+                ClashrupConfigError::RemoteConfigUrlMissingError => {
+                    println!(
+                        "{} Missing {}",
+                        "error:".red(),
+                        "remote_config_url".underline()
+                    );
+                }
+            }
+            return;
+        }
+    };
 
     match &args.command {
         Some(Commands::Setup) => {
@@ -124,7 +61,7 @@ fn main() {
             let clash_config_path = String::from("config.yaml");
 
             download_file(&config.remote_clash_binary_url, &clash_gzipped_path);
-            extract_gzip(&clash_gzipped_path, &clash_binary_path);
+            extract_gzip(&clash_gzipped_path, &clash_binary_path, &prefix);
             download_file(&config.remote_config_url, &clash_config_path);
         }
         None => {
---
+++ src/utils.rs
@@ -0,0 +1,91 @@
+use colored::*;
+use flate2::read::GzDecoder;
+use serde::{Deserialize, Serialize};
+use std::{fs, io, path::Path};
+use toml;
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub remote_clash_binary_url: String,
+    pub remote_config_url: String,
+    pub clash_config_root: String,
+}
+
+pub fn setup_default_config(path: &String) {
+    let default_config = Config {
+        remote_clash_binary_url: String::from(""),
+        remote_config_url: String::from(""),
+        // Reference to clash config: https://github.com/Dreamacro/clash/wiki/configuration
+        clash_config_root: String::from("~/.config/clash"),
+    };
+    let config = toml::to_string(&default_config).unwrap();
+    fs::write(path, config).unwrap();
+}
+
+pub fn parse_config(path: &String) -> Config {
+    let config = fs::read_to_string(path).unwrap();
+    let config: Config = toml::from_str(&config).unwrap();
+    config
+}
+
+pub fn download_file(url: &String, path: &String) {
+    println!(
+        "{} Downloading from {}",
+        "download:".blue(),
+        url.underline().yellow()
+    );
+    let mut resp = reqwest::blocking::get(url).unwrap();
+    let mut file = fs::File::create(path).unwrap();
+    resp.copy_to(&mut file).unwrap();
+    println!(
+        "{} Downloaded to {}",
+        "download:".blue(),
+        path.underline().yellow()
+    );
+}
+
+pub fn extract_gzip(gzip_path: &String, filename: &String, prefix: &str) {
+    let mut archive = GzDecoder::new(fs::File::open(gzip_path).unwrap());
+    let mut file = fs::File::create(filename).unwrap();
+    io::copy(&mut archive, &mut file).unwrap();
+    fs::remove_file(gzip_path).unwrap();
+    println!(
+        "{} Extracted to {}",
+        prefix.green(),
+        filename.underline().yellow()
+    );
+}
+
+#[derive(Debug)]
+pub enum ClashrupConfigError {
+    ConfigMissingError,
+    RemoteClashBinaryUrlMissingError,
+    RemoteConfigUrlMissingError,
+}
+
+pub fn validate_clashrup_config(
+    path: &String,
+    prefix: &str,
+) -> Result<Config, ClashrupConfigError> {
+    // Create clashrup default config if not exists
+    let config_path = Path::new(path);
+    if !config_path.exists() {
+        setup_default_config(path);
+        return Err(ClashrupConfigError::ConfigMissingError);
+    }
+
+    // Parse config file and validate if urls are defined
+    println!(
+        "{} Reading config file {}",
+        prefix.cyan(),
+        path.underline().yellow()
+    );
+    let config = parse_config(path);
+    if config.remote_clash_binary_url.is_empty() {
+        return Err(ClashrupConfigError::RemoteClashBinaryUrlMissingError);
+    }
+    if config.remote_config_url.is_empty() {
+        return Err(ClashrupConfigError::RemoteConfigUrlMissingError);
+    }
+    return Ok(config);
+}
