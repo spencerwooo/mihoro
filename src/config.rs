bbf248b147e79ffa83ba5bf90289f3721bb1471a
spencerwooo <spencer.woo@outlook.com>
2023-01-04 00:23:15 +0800

    rewrite config impl

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,9 @@
-use colored::*;
-use serde::{Deserialize, Serialize};
-use std::{fs, path::Path};
+use std::fs;
+use std::path::Path;
+
+use colored::Colorize;
+use serde::Deserialize;
+use serde::Serialize;
 use toml;
 
 #[derive(Serialize, Deserialize)]
@@ -22,33 +25,46 @@ impl Config {
             user_systemd_root: String::from("~/.config/systemd/user"),
         }
     }
-}
 
-pub fn setup_default_config(path: &str) {
-    let default_config = Config::new();
-    let config = toml::to_string(&default_config).unwrap();
-    fs::write(path, config).unwrap();
-}
+    /// Read raw config string from path and parse with crate toml
+    ///
+    /// TODO: Currently this will return error that shows a missing field error when parse fails, however the error
+    /// message always shows the line and column number as `line 1 column 1`, which is because the function
+    /// `fs::read_to_string` preserves newline characters as `\n`, resulting in a single-lined string.
+    pub fn setup_from(path: &str) -> Result<Config, toml::de::Error> {
+        let raw_config = fs::read_to_string(path).unwrap();
+        toml::from_str(&raw_config)
+    }
 
-pub fn parse_config(path: &str) -> Config {
-    let config = fs::read_to_string(path).unwrap();
-    let config: Config = toml::from_str(&config).unwrap();
-    config
+    /// Write config to path
+    pub fn write(&mut self, path: &Path) {
+        let serialized_config = toml::to_string(&self).unwrap();
+        fs::write(path, serialized_config).unwrap();
+    }
 }
 
 #[derive(Debug)]
-pub enum ClashrupConfigError {
-    ConfigMissingError,
-    RemoteClashBinaryUrlMissingError,
-    RemoteConfigUrlMissingError,
+pub enum ConfigError {
+    FileMissing,
+    ParseError,
 }
 
-pub fn validate_clashrup_config(path: &str, prefix: &str) -> Result<Config, ClashrupConfigError> {
+/// Tries to parse clashrup config as toml from path
+///
+/// * If config file does not exist, creates default config file to path and returns error
+/// * If found, tries to parse config file and returns error if parse fails or some fields are not defined
+pub fn parse_config(path: &str, prefix: &str) -> Result<Config, ConfigError> {
     // Create clashrup default config if not exists
     let config_path = Path::new(path);
     if !config_path.exists() {
-        setup_default_config(path);
-        return Err(ClashrupConfigError::ConfigMissingError);
+        Config::new().write(config_path);
+        println!(
+            "{} Created default config at {}, edit as needed",
+            prefix.yellow(),
+            path.underline()
+        );
+        println!("{} Run again to finish setup", prefix.yellow());
+        return Err(ConfigError::FileMissing);
     }
 
     // Parse config file and validate if urls are defined
@@ -57,12 +73,22 @@ pub fn validate_clashrup_config(path: &str, prefix: &str) -> Result<Config, Clas
         prefix.cyan(),
         path.underline().yellow()
     );
-    let config = parse_config(path);
-    if config.remote_clash_binary_url.is_empty() {
-        return Err(ClashrupConfigError::RemoteClashBinaryUrlMissingError);
-    }
-    if config.remote_config_url.is_empty() {
-        return Err(ClashrupConfigError::RemoteConfigUrlMissingError);
-    }
-    return Ok(config);
+
+    match Config::setup_from(path) {
+        Ok(config) => {
+            if config.remote_clash_binary_url.is_empty() {
+                println!("{} `remote_clash_binary_url` undefined", "error:".red());
+                return Err(ConfigError::ParseError);
+            }
+            if config.remote_config_url.is_empty() {
+                println!("{} `remote_config_url` undefined", "error:".red());
+                return Err(ConfigError::ParseError);
+            }
+            return Ok(config);
+        }
+        Err(error) => {
+            println!("{} {}", "error:".red(), error);
+            return Err(ConfigError::ParseError);
+        }
+    };
 }
